adea83a9f565d79c09f93ee2b68b43c2b225263f
ow-tech <wagwanwekon@outlook.com>
2020-09-28 15:16:09 +0300

    modal forms for taking order

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
+// user interface
+
 $(document).ready(function(){
     $('.card').hover(function(){
         $('.details', this).toggle(2000)
     });
 });
+
+
+// Business logic
+function myPizza(pizzaType, psize, crust, toppings, number, deliveryStatus){
+    this.pizzaType=pizzaType;
+    this.psize=psize;
+	this.crust=crust;
+	this.toppings=toppings;
+	this.number=number;
+	this.deliveryStatus=deliveryStatus;
+}
+
+function startOrder(){
+    
+    // swal({
+    //     title: "Finalize Your Order; If Delivery, Cost's 200 more",
+    //     content:{
+    //         element: "select",
+    //         attribute:
+    //     }
+    //     text: "Once deleted, you will not be able to recover this imaginary file!",
+        
+    //     icon: "info",
+    //     buttons: true,
+    //   })
+    //   .then((willDelete) => {
+    //     if (willDelete) {
+    //       swal("Poof! Your imaginary file has been deleted!", {
+    //         icon: "success",
+    //       });
+    //     } else {
+    //       swal("Your imaginary file is safe!");
+    //     }
+    //   });
+}
