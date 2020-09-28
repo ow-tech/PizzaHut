73f18f71dd58c94f95b3f9e060635067b04c4968
ow-tech <wagwanwekon@outlook.com>
2020-09-28 20:08:32 +0300

    styling modal inputs, bug handling

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ $(document).ready(function(){
 
 
 // Business logic
+
+
 function myPizza(pizzaType, psize, crust, toppings, number, deliveryStatus){
     this.pizzaType=pizzaType;
     this.psize=psize;
@@ -19,34 +21,27 @@ function myPizza(pizzaType, psize, crust, toppings, number, deliveryStatus){
 
 $(document).ready(function(){
     $('#makeOrder').click(function(){
-        let pizzaType = $('input[type="radio"]:checked').val();
+        let pizzaType = $('input').val();
         let psize =$(".psize[type='radio']:checked").val();
         let crust = $(".custom-select option:selected").val();
         let toppings =$ ("input[type='checkbox']:checked").val();
-		let number =$("#getnumber option:selected").val();
-        let deliveryStatus = $(".delivered").val();
-        
-        let total= (parseInt(psize) + parseInt(crust) + parseInt(toppings))*parseInt(number);
-        event.preventDefault();
-        if (deliveryStatus ==='200'){
-            let location = prompt('Where is convenient location for Delivery')
-            if (location == ''){
-                swal({
-                    title: 'Location is a Must',
-                    text:'Please Enter Your Location !!!\n By clicking ok Button Again',
-                    icon:'error'
-                })
+        let number =$("#getnumber option:selected").val();
+        let delivery= $(".delivered[type='checkbox']:checked ").val();
+        let deliveryStatus = parseInt(delivery);
 
-            } else{
-                swal({
-                    title: "Good job!. Order Completed",
-                    text: `Your order will be delivered to ${location} in about 1 hour.\n Total Amount is ksh. ${total+deliveryStatus} + Delivery`,
-                    icon: "success",
-                  })}
-        }
-        else{
+        let total= (parseInt(psize) + parseInt(crust) + parseInt(toppings))*parseInt(number)
+        event.preventDefault()
+        let location = prompt('Where is convenient location for Delivery')
+        if (location == ''){
+            swal({
+                title: 'Location is a Must',
+                text:'Please Enter Your Location !!!\n By clicking ok Button Again',
+                icon:'error'});
+        }else{
             swal({
                 title: "Good job!. Order Completed",
-                text: `Your order will be Ready ${location}  in about 30mins \n Total Amount is ksh. ${total}`})}
-    })
+                text: `Your order will be delivered to ${location} in about 1 hour.\n Total Amount is ksh. ${total+deliveryStatus} inclusive of Delivery cost`,
+                icon: "success",
+                });}
+    });
 });
